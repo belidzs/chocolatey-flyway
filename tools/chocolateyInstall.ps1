2993930c877dff52ec1c056944ccc6dc7b84ac29
Balázs Keresztury <b@belidzs.hu>
2019-09-23 15:15:30 +0200

    Initial commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$version = '6.0.4'
+$packageName = 'flyway.commandline'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/$version/flyway-commandline-$version.zip"
+$checksumType = 'sha256'
+$checksum = 'fcb4a1f5a6918a2376954d536f9539b77c943965d76ba7381baf09a855d0734d'
+Install-ChocolateyZipPackage $packageName $url $toolsDir -Checksum $checksum -ChecksumType $checksumType
+Install-BinFile "flyway" "$toolsDir\flyway-$version\flyway.cmd"
