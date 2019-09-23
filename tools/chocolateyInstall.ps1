7ae2c468a80a635efd783cb1730825ac2a48f89d
Balázs Keresztury <b@belidzs.hu>
2019-09-23 15:22:44 +0200

    gitignore fix

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,6 +3,6 @@ $packageName = 'flyway.commandline'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/$version/flyway-commandline-$version.zip"
 $checksumType = 'sha256'
-$checksum = 'fcb4a1f5a6918a2376954d536f9539b77c943965d76ba7381baf09a855d0734d'
+$checksum = 'a9867345cf8381c652d54a00907245567b1bc909e35e3375cfcd337561c013aa'
 Install-ChocolateyZipPackage $packageName $url $toolsDir -Checksum $checksum -ChecksumType $checksumType
 Install-BinFile "flyway" "$toolsDir\flyway-$version\flyway.cmd"
