7dcd4520befa0a4a4a1440756cfaef1ebe751db4
Balázs Keresztury <b@belidzs.hu>
2019-10-11 16:08:55 +0200

    Bugfix upgrade to 6.0.6

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
-﻿$version = '6.0.5'
+﻿$version = '6.0.6'
 $packageName = 'flyway'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/$version/flyway-commandline-$version.zip"
 $checksumType = 'sha256'
-$checksum = 'c5070c905e739f96e84f21b9a330596010f811cfe8f4e0302084a9d264968e99'
+$checksum = '1896e8bd1542cad1b4d1a6a556ba9aad9819a5aea7f10146e04ebbadbc0b8487'
 Install-ChocolateyZipPackage $packageName $url $toolsDir -Checksum $checksum -ChecksumType $checksumType
 Install-BinFile "flyway" "$toolsDir\flyway-$version\flyway.cmd"
